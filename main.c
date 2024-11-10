03128fee38c20408687eba413aee3c8d54ce6902
Gustavo Carvalho <gustavo54535251@gmail.com>
2024-11-10 09:44:29 -0300

    3

--- main.c
+++ main.c
@@ -1,10 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void imprime_histograma(int* vendas[],int num_categorias);
+void imprime_linha(int *venda);
 
 int main(){
+    int qtd, i;
+    scanf("%d",&qtd);
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    getchar();
     
+    if(qtd == 0){
+        printf("vazio");
+        return 0;
+    }
+
+    int **vetor = (int**)malloc(qtd * sizeof(int*));
+
+    for (i = 0; i < qtd; i++)
+    {
+        vetor[i] = (int*)malloc(qtd * sizeof(int));
+    }
+
+    for (i = 0; i < qtd; i++)
+    {
+        scanf("%d",vetor[i]);
+    }
+
+    imprime_histograma(vetor,qtd);
+
+    for (i = 0; i < qtd; i++)
+    {
+        free(vetor[i]);
+    }
+
+    free(vetor);
+
     return 0;
+}
+
+void imprime_histograma(int *vendas[],int num_categorias){
+    int i;
+    for(i = 0;i < num_categorias;i++){
+        imprime_linha(vendas[i]);
+    }
+}
+
+void imprime_linha(int *venda){
+    int i;
+    printf("%d ",*venda);
+    for(i = 0; i < *venda;i++){
+        printf("*");
+    }
+    printf("\n");
 }
